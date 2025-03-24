274de56ec42cc12bd334dc92f9bcb5bd00d91026
vm3do <medoavvadi@gmail.com>
2025-03-24 18:33:22 +0000

    quote crud ( store )

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quote;
+use Illuminate\Http\Request;
+
+class QuoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Quote::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $vaidated = $request->validate([
+            'title' => 'required|min:2|max:255',
+            'body' => 'required',
+        ]);
+
+        $quote = Quote::create($vaidated);
+
+        return $quote;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Quote $quote)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Quote $quote)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Quote $quote)
+    {
+        //
+    }
+}
